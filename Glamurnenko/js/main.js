057f900b8c18081db2d2d4934a0c98fd110cc121
Maxim Makarevich <113656036+maximMakarevich01@users.noreply.github.com>
2022-11-27 15:43:35 +0300

    Delete Glamurnenko directory

--- Glamurnenko/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", (e) => {
-  let navbar = document.getElementById("navbar").classList;
-  let active_class = "navbar_scrolled";
-
-  if (scrollY > 20) navbar.add(active_class);
-  else navbar.remove(active_class);
-});
